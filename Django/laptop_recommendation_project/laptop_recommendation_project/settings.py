88533555618e862c08c34febebeb74b5a7deaacb
Traveller <msafirisb@gmail.com>
2024-08-02 19:28:49 +0300

    updated commit

--- Django/laptop_recommendation_project/laptop_recommendation_project/settings.py
+++ Django/laptop_recommendation_project/laptop_recommendation_project/settings.py
@@ -14,12 +14,8 @@ from pathlib import Path
 from corsheaders.defaults import default_headers
 import django_heroku
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -30,7 +26,7 @@ SECRET_KEY = 'django-insecure-7_$!ld)5ff++u&u6s%bazh%w$^ut(hob%d_+e&lieopp%c44+4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -138,6 +134,8 @@ USE_TZ = True
 
 import os
 
+STATIC_URL = 'static/'
+
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -146,11 +144,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
